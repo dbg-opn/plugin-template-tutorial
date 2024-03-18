0de4a1f76c7efd46d3a38d56fbd4b67a1079383b
dbg-opn <eryk.grzeskow@gmail.com>
2024-03-19 00:03:00 +0800

    Initial commit

---
+++ Piggy Overlay.java
@@ -0,0 +1,32 @@
+package ${PACKAGE_NAME}.${PLUGIN_NAME};
+
+
+import com.example.EthanApiPlugin.Collections.TileObjects;
+import net.runelite.api.*;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.ui.overlay.*;
+
+import javax.inject.Inject;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.Optional;
+
+public class ${PLUGIN_NAME}Overlay extends OverlayPanel {
+
+    private final Client client;
+    private final ${PLUGIN_NAME}Plugin plugin;
+
+    @Inject
+    private ${PLUGIN_NAME}Overlay(Client client, ${PLUGIN_NAME}Plugin plugin) {
+        this.client = client;
+        this.plugin = plugin;
+        setPosition(OverlayPosition.DYNAMIC);
+        setLayer(OverlayLayer.ABOVE_SCENE);
+
+    }
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        return super.render(graphics);
+    }
+}
